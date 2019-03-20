8381514988649634cf699a624745827a31adf7b4
Rainer <rainer@websimple.be>
2019-03-20 15:53:06 +0100

    impot fabrique

---
+++ interface_traduction_objets_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Interface de traduction pour objets
+ *
+ * @plugin     Interface de traduction pour objets
+ * @copyright  2019
+ * @author     RAiner
+ * @licence    GNU/GPL
+ * @package    SPIP\Interface_traduction_objets\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
