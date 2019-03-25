befb3f42ca554cee6788e4a7028e9076bf95b3a5
Rainer <rainer@websimple.be>
2019-03-25 15:00:49 +0100

    calcul des rubriques parentes si secteur par langue

--- interface_traduction_objets_pipelines.php
+++ interface_traduction_objets_pipelines.php
@@ -15,6 +15,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 
 function interface_traduction_objets_recuperer_fond($flux) {
+
 	//Insertion des onglets de langue
 	$contexte = $flux['args']['contexte'];
 	$fond = $flux['args']['fond'];
@@ -31,23 +32,46 @@ function interface_traduction_objets_recuperer_fond($flux) {
 	) {
 
 		if ($fond == 'prive/squelettes/contenu/' . $objet) {
+
 			$langues_dispos = explode(',', $GLOBALS['meta']['langues_multilingue']);
 			$table_objet = $objet_exec['table_objet_sql'];
 
+			$select = ['id_trad', 'lang', $id_table_objet];
+
+			$id_parent_table = '';
+			if (isset($desc['field']['id_rubrique'])) {
+				if ($objet != 'rubrique') {
+					$id_parent_table = 'id_rubrique';
+				}
+				else {
+					$id_parent_table = 'id_parent';
+				}
+				$select[] = $id_parent_table;
+			}
+
 			$donnees_objet = sql_fetsel(
-				'id_trad,lang,' . $id_table_objet,
+				$select,
 				$table_objet,
 				$id_table_objet . '=' . $contexte[$id_table_objet]);
 
+			$id_trad_parent = '';
+			if ($id_parent_table) {
+				$rubrique_parent = sql_fetsel(
+					'id_trad, id_rubrique',
+					'spip_rubriques',
+					'id_rubrique=' . $donnees_objet[$id_parent_table]);
+
+					$id_trad_parent = $rubrique_parent['id_rubrique'];
+			}
+
 
 			$lang_objet = $donnees_objet['lang'];
-
 			$id_trad = $donnees_objet['id_trad'];
 			$langues_traduites = [];
+
 			if ($id_trad > 0) {
 				$trad_new = FALSE;
-				$langues_traduites[$lang_objet] = $lang_objet;
-
+				$langues_traduites[$lang_objet] = $id_objet;
 				$traductions = sql_allfetsel(
 					'lang,' . $id_table_objet,
 					$table_objet,
@@ -55,7 +79,6 @@ function interface_traduction_objets_recuperer_fond($flux) {
 
 				foreach ($traductions AS $traduction) {
 					$langues_traduites[$traduction['lang']] = $traduction[$id_table_objet];
-
 				}
 			}
 			else {
@@ -65,22 +88,27 @@ function interface_traduction_objets_recuperer_fond($flux) {
 
 			$contexte['objet'] = $objet;
 			$contexte['id_objet'] = $id_objet;
+
+			// Si secteur par langue, on établit l'id_parent.
+			if (test_plugin_actif('secteur_langue')) {
+				$contexte['id_parent'] = isset($donnees_objet['id_rubrique']) ?
+					$donnees_objet['id_rubrique'] :
+					(isset($donnees_objet['id_parent']) ? $donnees_objet['id_parent'] : '');
+			}
 			$contexte['id_table_objet'] = $id_table_objet;
 			$contexte['langues_dispos'] = $langues_dispos;
 			$contexte['lang_objet'] = $lang_objet;
 			$contexte['id_trad'] = $id_trad;
+			$contexte['id_trad_parent'] = $id_trad_parent;
 			$contexte['langues_traduites'] = $langues_traduites;
 
 
 			$barre_langue = recuperer_fond("prive/inclure/barre_traductions_objet", $contexte, array('ajax' => true));
-				$flux['data']['texte'] = str_replace('</h1>', '</h1>' . $barre_langue, $flux['data']['texte']);
-
+			$flux['data']['texte'] = str_replace('</h1>', '</h1>' . $barre_langue, $flux['data']['texte']);
 		}
 	}
-
 	return $flux;
 }
-
 
 function interface_traduction_objets_header_prive($flux) {
 	$flux .= '<link rel="stylesheet" href="' . find_in_path('css/interface_traduction_objets_styles.css') . '" type="text/css" media="all" />';
@@ -91,11 +119,15 @@ function interface_traduction_objets_header_prive($flux) {
 /*Ajoute la langue de traduction dans le chargement du formulaire edition_rubrique*/
 function interface_traduction_objets_formulaire_charger($flux) {
 	$form = $flux['args']['form'];
+	$segments = explode('_', $form);
 
-	if ($form == 'editer_rubrique' AND _request('new') == 'oui') {
+	if ($segments[0] == 'editer' AND
+		_request('new') == 'oui' AND
+		isset($segments[1]) AND
+		$table_objet = table_objet($segments[1])) {
 
-		if (!$flux['data']['lang_dest'] = _request('lang_dest') AND $id_rubrique = _request('id_parent')) {
-			$flux['data']['lang_dest'] = sql_getfetsel('lang', 'spip_rubriques', 'id_rubrique=' . $id_rubrique);
+		if (!$flux['data']['lang_dest'] = _request('lang_dest') AND $id_parent = _request('id_parent')) {
+			$flux['data']['lang_dest'] = sql_getfetsel('lang', 'spip_rubriques', 'id_rubrique=' . $id_parent);
 		}
 		if (isset($flux['data']['lang_dest'])) {
 			$flux['data']['_hidden'] .= '<input type="hidden" name="lang_dest" value="' . $flux['data']['lang_dest'] . '"/>';
@@ -108,14 +140,11 @@ function interface_traduction_objets_formulaire_charger($flux) {
 
 /*Prise en compte de la langue de traduction dans le traitement du formulaire edition_article*/
 function interface_traduction_objets_pre_insertion($flux) {
-	if ($flux['args']['table'] == 'spip_rubriques') {
-
-		if ($lang = _request('lang_dest')) {
-			$id_trad = _request('lier_trad');
-			$flux['data']['lang'] = $lang;
-			$flux['data']['langue_choisie'] = 'oui';
-			$flux['data']['id_trad'] = $id_trad;
-		}
+	if ($lang = _request('lang_dest') AND $id_trad = _request('lier_trad')) {
+		$flux['data']['lang'] = $lang;
+		$flux['data']['langue_choisie'] = 'oui';
+		$flux['data']['id_trad'] = $id_trad;
+		spip_log($flux, 'teste');
 	}
 	return $flux;
 }
